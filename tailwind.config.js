2c5a57891365b9bde854c6723421ea02c37dd756
Favourdev1 <favourapps17@gmail.com>
2024-05-12 08:25:44 +0100

    chore: Update Nav component and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
 
-  theme: {
-    extend: {
-      // backgroundImage: {
-      //   "Footer-Bg": "url('/images/Rectangle.png')",
-      // },
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+
+    ],
+
+    theme: {
+        extend: {
+            // backgroundImage: {
+            //   "Footer-Bg": "url('/images/Rectangle.png')",
+            // },
+        },
     },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+
 };
