9371f5d77dc6f9fc9fe6adae47c78a1a0462094d
Favourdev1 <favourapps17@gmail.com>
2024-05-12 23:10:54 +0100

    minor spelling fixed

--- src/components/AppComponents/Layout/Mainlayout.jsx
+++ src/components/AppComponents/Layout/Mainlayout.jsx
@@ -11,53 +11,53 @@ const Mainlayout = () => {
   const products = [
     {
       title: "Female Sports Wear",
-      img: "./Images/Well-builtWoman.png",
+      img: "./images/Well-builtWoman.png",
       price: "N30,000",
     },
     {
       title: "TPS5 Gaming Dual Pad ",
-      img: "./Images/GameController.png",
+      img: "./images/GameController.png",
       price: "N15,000",
     },
     {
       title: "Ijaw Traditional wear",
-      img: "./Images/AfricanMan.png",
+      img: "./images/AfricanMan.png",
       price: "N8,500",
     },
     {
       title: "Male underwear",
-      img: "./Images/Underware.png",
+      img: "./images/Underware.png",
       price: "N5,000",
     },
     {
       title: "Nike Airforce Jordan 1s",
-      img: "./Images/NewSneakers.png",
+      img: "./images/NewSneakers.png",
       price: "N30,000",
     },
     {
       title: "Sony Gaming Headset",
-      img: "./Images/BlackHeadphone.png",
+      img: "./images/BlackHeadphone.png",
       price: "N21,000",
     },
     // 5 dummy products 
     {
       title: "Product 1",
-      img: "./Images/Product1.png",
+      img: "./images/Product1.png",
       price: "N10,000",
     },
     {
       title: "Product 2",
-      img: "./Images/Product2.png",
+      img: "./images/Product2.png",
       price: "N12,000",
     },
     {
       title: "Product 3",
-      img: "./Images/Product3.png",
+      img: "./images/Product3.png",
       price: "N8,000",
     },
     {
       title: "Product 4",
-      img: "./Images/Product4.png",
+      img: "./images/Product4.png",
       price: "N15,000",
     },
  
